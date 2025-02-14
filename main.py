2dca107d52ac613d789d462fccdf2d8bcb5c6884
Gayrat Rahmatulayev <gayratrahmatulayev8@gmail.com>
2025-02-15 00:57:14 +0300

    class write_state added and guessing system

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
-import turtle
+import turtle, pandas
+from write_state import WriteState
 
-game_screen = turtle.Screen()
+# create a screen
+screen = turtle.Screen()
+screen.title("U.S. States Game")
 usa_image = r"./data/blank_states_img.gif"
 print(usa_image)
-game_screen.addshape(usa_image)
-game_screen.setup(735,495)
+screen.addshape(usa_image)
+screen.setup(735, 495)
 turtle.shape(usa_image)
 
+# read data from file and save to a list
+data = pandas.read_csv("./data/50_states.csv")
+state_list = data["state"].to_list()
+ycor_list = data["y"].to_list()
+xcor_list = data["x"].to_list()
 
+score = 0
+while score != 50:
+    answer = screen.textinput(prompt="What's another state name?", title=f"{score}/50 States Correct").title()
+    for index in range(len(state_list)):
+        if answer == state_list[index]:
+            score += 1
+            text = state_list[index]
+            x = xcor_list[index]
+            y = ycor_list[index]
 
+    write_state = WriteState(text, x, y)
 
+screen.exitonclick()
 
-game_screen.exitonclick()
-
-# Get Coordinates
+# Get Coordinates first
 # def get_mouse_click_coor(x,y):
 #     print(x,y)
 # turtle.onscreenclick(get_mouse_click_coor)
---
+++ write_state.py
@@ -0,0 +1,16 @@
+from turtle import Turtle
+
+
+class WriteState(Turtle):
+    def __init__(self, text, x, y):
+        super().__init__()
+        self.text = text
+        self.x = x
+        self.y = y
+        self.make_text(self.text, self.x, self.y)
+
+    def make_text(self, text, x, y):
+        self.hideturtle()
+        self.penup()
+        self.goto(x, y)
+        self.write(arg=text, font=("Arial", 8, "bold"))
